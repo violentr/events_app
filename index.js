42de27659c53698f98841317699fa2afc4abe225
Deniss <den.b@itservicesgroup.co.uk>
2018-01-13 18:26:15 +0000

    Add Helper list, renders names of drinks and ids

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ var url = 'https://mock-api.drinks.test.siliconrhino.io/events'
 app.set('view engine', 'hbs')
 app.use(express.static(__dirname + '/public'));
 
+hbs.registerHelper('list', (items, options) => {
+  var out = ""
+
+  for(var i=0, l=items.length; i<l; i++) {
+    out = out + `<li><a href=/${i+1}>` + options.fn(items[i]) + "</a></li>";
+  }
+  return out
+});
+
 app.get('/', (req, res) => {
   request(url, (error, response, body) => {
     if(error) {
@@ -21,10 +30,17 @@ app.get('/', (req, res) => {
     });
   })
 })
-app.get('/', (req, res) => {
+app.get('/:id', (req, res) => {
+  var params = req.params.id
+  request(url + `/${params}`, (error, response, body) => {
+    if(error) {
+      body = 'something went wrong'
+    }
   res.render('home.hbs', {
-    pageTitle: 'Drinks'
+    pageTitle: 'Drinks',
+    body: body
   });
+ })
 })
 
 app.listen(3000)
