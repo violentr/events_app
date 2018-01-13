fc456a924d1161f7ad4777fc91e90ebb638d0696
Deniss <den.b@itservicesgroup.co.uk>
2018-01-13 17:09:14 +0000

    Add Make http request to Api url, reads json data

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const hbs = require('hbs');
+const request = require('request');
 var app = express();
+
+var url = 'https://mock-api.drinks.test.siliconrhino.io/events'
 
 app.set('view engine', 'hbs')
 app.use(express.static(__dirname + '/public'));
 
+app.get('/', (req, res) => {
+  request(url, (error, response, body) => {
+    if(error) {
+      body = 'something went wrong'
+    }
+    body = JSON.parse(body)
+    console.log("JSON", body)
+    res.render('home.hbs', {
+      pageTitle: 'Drinks',
+      body: body
+    });
+  })
+})
 app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Drinks'
